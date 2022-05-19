b435321eba5aaf3b2c41245071da2b862379691f
Aminullah Jalal <60218423+aminullahdeveloper@users.noreply.github.com>
2022-05-19 22:11:45 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import { useState } from "react";
-import './style/style.scss'
-
+import { useState } from 'react';
+import './style/style.scss';
 
 function App() {
-  const[count, setCount] =useState(0);
+  const [count, setCount] = useState(0);
   return (
-    <div className="container">
-      <div className="card text-center">
-        <div className="card-body">
+    <div className='counter-cover'>
+      <div className='counter-container'>
+        <div>
           <h1>Counter App</h1>
-          <div>
-            <h2>{count}</h2>
-            <button className="button1"  
-            onClick ={()=> setCount(count+1)}>Increment 
-            </button>
-            <button className="button2"    
-            onClick={()=> setCount(count-1)}>Deccrement 
-            </button>
-            <button className="button3"  
-            onClick={()=> setCount(0)}>Reset 
-            </button>
-          </div>
         </div>
-
+        <div>
+          <h2>{count}</h2>
+        </div>
+        <div>
+          <button
+            className='counter-btn increment-btn'
+            onClick={() => setCount(count + 1)}
+          >
+            Increment
+          </button>
+          <button
+            className='counter-btn decrement-btn'
+            onClick={() => setCount(count - 1)}
+          >
+            Decrement
+          </button>
+          <button className='counter-btn reset-btn' onClick={() => setCount(0)}>
+            Reset
+          </button>
+        </div>
       </div>
     </div>
   );
